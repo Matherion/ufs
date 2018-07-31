888f68618016a0eeb86b9db7d17b919329ca7ed9
Gjalt-Jorn Peters <gjalt-jorn@greatergood.eu>
2018-07-31 18:27:01 +0200

    Put %IN% back

--- R/IN.R
+++ R/IN.R
@@ -1,13 +1,13 @@
-#' Case insensitive version of \code{\%in\%}
-#'
-#' @usage find \%IN\% table
+#' Case insensitive version of \%in\%
 #'
 #' @param find The element(s) to look up in the vector or matrix.
 #' @param table The vector or matrix in which to look up the element(s).
 #'
+#' @usage find \%IN\% table
+#'
 #' @return A logical vector.
-#' @export
 #' @rdname IN
+#' @export
 #'
 #' @examples letters[1:4] %IN% LETTERS
 "%IN%" <- function(find, table) {
